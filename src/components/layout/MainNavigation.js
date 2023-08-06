1040a61b559008b5fe98a9e2aba1765c670df52f
helena nora <eithazinkhaing10@gmail.com>
2023-08-06 17:49:00 +0630

    React Simple Project

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,31 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import './MainNavigation.css';
+import FavoritesContext from '../../store/favorites-context';
+
+function MainNavigation() {
+    const favoritesCtx = useContext(FavoritesContext);
+    return (
+        <header className='header'>
+            <div className='logo'>React Meetups</div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>All Meetups</Link>
+                    </li>
+                    <li>
+                        <Link to='/new-meetup'>New Meetup</Link>
+                    </li>
+                    <li>
+                        <Link to='/favorites'>
+                            My Favorites
+                            <span className='badge'>{ favoritesCtx.totalFavorites}</span>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+    
+}
+export default MainNavigation;
---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,40 @@
+import { useContext } from 'react';
+import Card from '../ui/Card';
+import './MeetupItem.css';
+import FavoritesContext from '../../store/favorites-context';
+
+function MeetupItem(props) {
+    const favoriteCtx = useContext(FavoritesContext);
+    const itemIsFavorite = favoriteCtx.itemIsFavorite(props.id);
+    function toggleFavoriteStatusHandler() {
+        if (itemIsFavorite) {
+            favoriteCtx.removeFavorite(props.id);
+        } else {
+            favoriteCtx.addFavorite({
+                id: props.id,
+                title: props.title,
+                description: props.description,
+                image: props.image,
+                address:props.address
+            });
+        }
+    }
+    return (
+        <li className='item'>
+            <Card>
+            <div className='image'>
+                <img src={props.image} alt={props.title} />
+            </div>
+            <div className='content'>
+                <h3>{props.title}</h3>
+                <address>{props.address}</address>
+                <p>{props.description}</p>
+            </div>
+            <div className='actions'>
+                    <button onClick={toggleFavoriteStatusHandler}>{ itemIsFavorite?'Remove From Favorites':'To Favorites'}</button>
+            </div>
+            </Card>
+        </li>
+    )
+}
+export default MeetupItem;
